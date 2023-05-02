e2547e77db1192e36b46f749c7bfd21b0edbf5d4
Rishikesh2744 <98737806+Rishikesh2744@users.noreply.github.com>
2023-05-02 22:32:42 +0530

    commit

--- C++ STL/hashmaps.cpp
+++ C++ STL/hashmaps.cpp
@@ -1,10 +1,59 @@
 #include<iostream>
 #include<map>
+#include<unordered_map>
 using namespace std;
 //insertion,deletion,searching O(1)
 
 int main()
 {
-    //
-    
+        //unordered map
+        unordered_map<string,int> m;
+
+        //insertion 1
+        pair<string,int> p= make_pair("babbar",3);
+        m.insert(p);
+
+        // insertion 2
+        pair<string,int> p2("love",2);
+        m.insert(p2);
+
+        //inserttion 3
+        m["mera"]=1;        //creation
+        m["mera"]=2;        //updation
+
+        //serach
+        cout<<m["mera"]<<endl;
+        cout<<m.at("babbar")<<endl;
+
+        //size
+        cout<<m.size()<<endl<<endl;
+
+        //to check presence
+        cout<<m.count("bro")<<endl<<endl;
+        cout<<m.count("mera")<<endl<<endl;
+
+
+        //traversal-1
+        for(auto it=m.begin();it!=m.end();it++)
+        {
+            cout<<it->first<<"-> "<<it->second<<endl;
+        }cout<<endl<<endl;
+
+        //traversal-2
+        for(auto i:m)
+        {
+            cout<<i.first<<"->"<<i.second<<endl;
+        }cout<<endl<<endl;
+
+        //traversal using iterator
+        unordered_map<string,int> :: iterator it=m.begin();
+        while(it!=m.end())
+        {
+            cout<<it->first<<"->"<<it->second<<endl;
+            it++;
+        }cout<<endl<<endl;
+        //
+
+
+        return 0;
 }
